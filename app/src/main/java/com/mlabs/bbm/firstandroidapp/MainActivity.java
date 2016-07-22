9effa2875f3789ad7ce10b6efbf836ccb9512f4f
AnicieteJ <jolo1110@yahoo.com>
2016-07-22 15:26:56 +0800

    Splash Screen and Login Screen w/ Validations

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -8,67 +8,58 @@ import android.view.View;
 import  android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 
 public class MainActivity extends ActionBarActivity {
 
-
-    EditText Uname;
-    EditText Pwd;
-    Button btnLogs;
-
-
+    EditText editText_username;
+    EditText editText_password;
+    Button button_submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText_username = (EditText) findViewById(R.id.editText_user);
+        editText_password = (EditText) findViewById(R.id.editText_password);
+        button_submit = (Button) findViewById(R.id.button_submit);
 
-    }
-
-    public void onButtonClick(View v)
-    {
-        if(v.getId() ==  R.id.btnLog)
-        {
-            EditText a = (EditText)findViewById(R.id.TFUname);
-            EditText p = (EditText)findViewById(R.id.TFPword);
-            Intent i = new Intent(MainActivity.this, Display.class);
-
-            String uname = a.getText().toString();
-            String pword = p.getText().toString();
-
-            boolean b = Pattern.matches("[a-zA-Z0-9]{4,16}", uname);
-            if(b == true)
-            {
-
-                i.putExtra("Username",uname);
-                startActivity(i);
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!validateUser(editText_username.getText().toString())){
+                    editText_username.setError("Invalid Email");
+                    editText_username.requestFocus();
+                } else if (!validatePassword(editText_password.getText().toString())){
+                    editText_password.setError("Invalid Password");
+                    editText_password.requestFocus();
+                } else {
+                    Toast.makeText(MainActivity.this, "Account Validated", Toast.LENGTH_LONG).show();
+                }
             }
-            else
-            {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage(" Username or Password Must Be :" +  "\n" +
-                        " ALPHANUMERIC" + "\n" +
-                        " 8 to 24 Characters");
-                builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
 
-                    }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();
-
+            private boolean validatePassword(String password) {
+                if(password!=null && password.length()>6){
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-    }
 
-    public void clickExit(View x)
-    {
-        finish();
+            private boolean validateUser(String user) {
+                String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+                Pattern pattern = Pattern.compile(emailPattern);
+                Matcher matcher = pattern.matcher(user);
+
+                return matcher.matches();
+            }
+        });
     }
 
 }
